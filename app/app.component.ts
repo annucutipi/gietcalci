cf68e34f24ce442844179b41cc68f0e3e0ecac72
annucutipi <129783676+annucutipi@users.noreply.github.com>
2023-04-04 07:14:13 +0530

    Add files via upload

---
+++ app/app.component.ts
@@ -0,0 +1,1086 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'comena';
+
+
+
+
+
+
+
+
+
+
+
+
+
+num1:string = '';
+num2:string = '';
+result:number=0;
+ 
+ 
+Total()
+{
+  this.result = parseFloat(this.num1) * parseFloat(this.num2);
+}
+
+
+num3:string = '';
+num4:string = '';
+result1:number=0;
+ 
+ 
+Total1()
+{
+  this.result1 = parseFloat(this.num3) * parseFloat(this.num4);
+
+}
+
+num5:string = '';
+num6:string = '';
+result2:number=0;
+ 
+ 
+Total2()
+{
+  this.result2 = parseFloat(this.num5) * parseFloat(this.num6);
+}
+
+
+num7:string = '';
+num8:string = '';
+result3:number=0;
+ 
+ 
+Total3()
+{
+  this.result3 = parseFloat(this.num7) * parseFloat(this.num8);
+}
+
+num9:string = '';
+num10:string = '';
+result4:number=0;
+ 
+ 
+Total4()
+{
+  this.result4 = parseFloat(this.num9) * parseFloat(this.num10);
+}
+
+
+
+num11:string = '';
+num12:string = '';
+result5:number=0;
+ 
+ 
+Total5()
+{
+  this.result5 = parseFloat(this.num11) * parseFloat(this.num12);
+}
+
+
+
+
+num13:string = '';
+num14:string = '';
+result6:number=0;
+ 
+ 
+Total6()
+{
+  this.result6 = parseFloat(this.num13) * parseFloat(this.num14);
+}
+
+num15:string = '';
+num16:string = '';
+result7:number=0;
+ 
+ 
+Total7()
+{
+  this.result7 = parseFloat(this.num15) * parseFloat(this.num16);
+}
+
+
+
+
+
+resultall:number=0;
+Totalall()
+{
+  this.resultall = this.result + this.result1 + this.result2 + this.result3 + this.result4 + this.result5 + this.result6 + this.result7;
+}
+
+
+
+
+
+
+
+resultfinal:number=0;
+Totalfinal()
+{
+  this.resultfinal = this.resultall/19;
+}
+
+num30:string = '';
+num31:string = '';
+result31:number=0;
+ 
+ 
+Total31()
+{
+  this.result31 = parseFloat(this.num30) * parseFloat(this.num31);
+}
+
+
+
+
+num32:string = '';
+num33:string = '';
+result33:number=0;
+ 
+ 
+Total33()
+{
+  this.result33 = parseFloat(this.num32) * parseFloat(this.num33);
+}
+
+
+
+
+num34:string = '';
+num35:string = '';
+result35:number=0;
+ 
+ 
+Total35()
+{
+  this.result35 = parseFloat(this.num34) * parseFloat(this.num35);
+}
+
+
+
+
+num36:string = '';
+num37:string = '';
+result37:number=0;
+ 
+ 
+Total37()
+{
+  this.result37 = parseFloat(this.num36) * parseFloat(this.num37);
+}
+
+
+
+
+
+
+
+num38:string = '';
+num39:string = '';
+result39:number=0;
+ 
+ 
+Total39()
+{
+  this.result39 = parseFloat(this.num38) * parseFloat(this.num39);
+}
+
+
+
+num40:string = '';
+num41:string = '';
+result41:number=0;
+ 
+ 
+Total41()
+{
+  this.result41 = parseFloat(this.num40) * parseFloat(this.num41);
+}
+
+
+
+
+num42:string = '';
+num43:string = '';
+result43:number=0;
+ 
+ 
+Total43()
+{
+  this.result43 = parseFloat(this.num42) * parseFloat(this.num43);
+
+}
+
+
+resultall2:number=0;
+
+Totalall2()
+{
+  this.resultall2 = this.result31 + this.result33 + this.result35 + this.result37 + this.result39 + this.result41 + this.result43;
+
+}
+
+resultfinal2:number=0;
+ 
+ 
+Totalfinal2()
+{
+  this.resultfinal2 = this.resultall2/18;
+
+}
+
+
+num60:string = '';
+num61:string = '';
+result61:number=0;
+ 
+ 
+Total61()
+{
+  this.result61 = parseFloat(this.num60) * parseFloat(this.num61);
+}
+
+num62:string = '';
+num63:string = '';
+result63:number=0;
+ 
+ 
+Total63()
+{
+  this.result63 = parseFloat(this.num62) * parseFloat(this.num63);
+}
+
+num64:string = '';
+num65:string = '';
+result65:number=0;
+ 
+ 
+Total65()
+{
+  this.result65 = parseFloat(this.num64) * parseFloat(this.num65);
+}
+
+
+num66:string = '';
+num67:string = '';
+result67:number=0;
+ 
+ 
+Total67()
+{
+  this.result67 = parseFloat(this.num66) * parseFloat(this.num67);
+}
+
+
+num68:string = '';
+num69:string = '';
+result69:number=0;
+ 
+ 
+Total69()
+{
+  this.result69 = parseFloat(this.num68) * parseFloat(this.num69);
+}
+
+
+
+num70:string = '';
+num71:string = '';
+result71:number=0;
+ 
+ 
+Total71()
+{
+  this.result71 = parseFloat(this.num70) * parseFloat(this.num71);
+}
+
+
+
+num72:string = '';
+num73:string = '';
+result73:number=0;
+ 
+ 
+Total73()
+{
+  this.result73 = parseFloat(this.num72) * parseFloat(this.num73);
+}
+
+
+num74:string = '';
+num75:string = '';
+result75:number=0;
+ 
+ 
+Total75()
+{
+  this.result75 = parseFloat(this.num74) * parseFloat(this.num75);
+}
+
+num76:string = '';
+num77:string = '';
+result77:number=0;
+ 
+ 
+Total77()
+{
+  this.result77 = parseFloat(this.num76) * parseFloat(this.num77);
+}
+
+
+
+num78:string = '';
+num79:string = '';
+result79:number=0;
+ 
+ 
+Total79()
+{
+  this.result79 = parseFloat(this.num78) * parseFloat(this.num79);
+}
+
+
+resultall3:number=0;
+ 
+ 
+Totalall3()
+{
+  this.resultall3 = this.result61 + this.result63 + this.result65 + this.result67 + this.result69 + this.result71 + this.result73 + this.result75 + this.result77 + this.result79;
+}
+
+
+resultfinal3:number=0;
+ 
+ 
+Totalfinal3()
+{
+  this.resultfinal3 = this.resultall3/21;
+}
+
+
+num80:string = '';
+num81:string = '';
+result81:number=0;
+ 
+ 
+Total81()
+{
+  this.result81 = parseFloat(this.num80) * parseFloat(this.num81);
+}
+
+
+num82:string = '';
+num83:string = '';
+result83:number=0;
+ 
+ 
+Total83()
+{
+  this.result83 = parseFloat(this.num82) * parseFloat(this.num83);
+}
+
+
+num84:string = '';
+num85:string = '';
+result85:number=0;
+ 
+ 
+Total85()
+{
+  this.result85 = parseFloat(this.num84) * parseFloat(this.num85);
+}
+
+
+num86:string = '';
+num87:string = '';
+result87:number=0;
+ 
+ 
+Total87()
+{
+  this.result87 = parseFloat(this.num86) * parseFloat(this.num87);
+}
+
+
+num88:string = '';
+num89:string = '';
+result89:number=0;
+ 
+ 
+Total89()
+{
+  this.result89 = parseFloat(this.num88) * parseFloat(this.num89);
+}
+
+
+num90:string = '';
+num91:string = '';
+result91:number=0;
+ 
+ 
+Total91()
+{
+  this.result91 = parseFloat(this.num90) * parseFloat(this.num91);
+}
+
+
+num100:string = '';
+num101:string = '';
+result101:number=0;
+ 
+ 
+Total101()
+{
+  this.result101 = parseFloat(this.num100) * parseFloat(this.num101);
+}
+
+
+num102:string = '';
+num103:string = '';
+result103:number=0;
+ 
+ 
+Total103()
+{
+  this.result103 = parseFloat(this.num102) * parseFloat(this.num103);
+}
+
+
+num104:string = '';
+num105:string = '';
+result105:number=0;
+ 
+ 
+Total105()
+{
+  this.result105 = parseFloat(this.num104) * parseFloat(this.num105);
+
+}
+
+
+resultall4:number=0;
+ 
+ 
+Totalall4()
+{
+  this.resultall4 = this.result81 + this.result83 + this.result85 + this.result87 + this.result89 + this.result91 + this.result101 + 
+  this.result103 + this.result105;
+}
+
+
+resultfinal4:number=0;
+ 
+ 
+Totalfinal4()
+{
+  this.resultfinal4 = this.resultall4/21;
+
+}
+
+
+num106:string = '';
+num107:string = '';
+result107:number=0;
+ 
+ 
+Total107()
+{
+  this.result107 = parseFloat(this.num106) * parseFloat(this.num107);
+
+}
+
+
+
+num108:string = '';
+num109:string = '';
+result109:number=0;
+ 
+ 
+Total109()
+{
+  this.result109 = parseFloat(this.num108) * parseFloat(this.num109);
+
+}
+
+
+num110:string = '';
+num111:string = '';
+result111:number=0;
+ 
+ 
+Total111()
+{
+  this.result111 = parseFloat(this.num110) * parseFloat(this.num111);
+
+}
+
+
+num112:string = '';
+num113:string = '';
+result113:number=0;
+ 
+ 
+Total113()
+{
+  this.result113 = parseFloat(this.num112) * parseFloat(this.num113);
+
+}
+
+
+num114:string = '';
+num115:string = '';
+result115:number=0;
+ 
+ 
+Total115()
+{
+  this.result115 = parseFloat(this.num114) * parseFloat(this.num115);
+
+}
+
+
+
+
+
+
+num116:string = '';
+num117:string = '';
+result117:number=0;
+ 
+ 
+Total117()
+{
+  this.result117 = parseFloat(this.num116) * parseFloat(this.num117);
+
+}
+
+num118:string = '';
+num119:string = '';
+result119:number=0;
+ 
+ 
+Total119()
+{
+  this.result119 = parseFloat(this.num118) * parseFloat(this.num119);
+
+}
+
+num200:string = '';
+num201:string = '';
+result201:number=0;
+ 
+ 
+Total201()
+{
+  this.result201 = parseFloat(this.num200) * parseFloat(this.num201);
+
+}
+
+num202:string = '';
+num203:string = '';
+result203:number=0;
+ 
+ 
+Total203()
+{
+  this.result203 = parseFloat(this.num202) * parseFloat(this.num203);
+
+}
+
+num204:string = '';
+num205:string = '';
+result205:number=0;
+ 
+ 
+Total205()
+{
+  this.result205 = parseFloat(this.num204) * parseFloat(this.num205);
+
+}
+
+resultall5:number=0;
+ 
+ 
+Totalall5()
+{
+  this.resultall5 = this.result107 + this.result109 + this.result111 + this.result113 + this.result115 + this.result117 + this.result119 + this.result201 + this.result203 + this.result205;
+
+}
+
+
+resultfinal5:number=0;
+ 
+ 
+Totalfinal5()
+{
+  this.resultfinal5 = this.resultall5/22;
+
+}
+
+
+num206:string = '';
+num207:string = '';
+result207:number=0;
+ 
+ 
+Total207()
+{
+  this.result207 = parseFloat(this.num206) * parseFloat(this.num207);
+
+}
+
+num208:string = '';
+num209:string = '';
+result209:number=0;
+ 
+ 
+Total209()
+{
+  this.result209 = parseFloat(this.num208) * parseFloat(this.num209);
+
+}
+
+num300:string = '';
+num301:string = '';
+result301:number=0;
+ 
+ 
+Total301()
+{
+  this.result301 = parseFloat(this.num300) * parseFloat(this.num301);
+
+}
+
+
+num302:string = '';
+num303:string = '';
+result303:number=0;
+ 
+ 
+Total303()
+{
+  this.result303 = parseFloat(this.num302) * parseFloat(this.num303);
+
+}
+
+num304:string = '';
+num305:string = '';
+result305:number=0;
+ 
+ 
+Total305()
+{
+  this.result305 = parseFloat(this.num304) * parseFloat(this.num305);
+
+}
+
+
+num306:string = '';
+num307:string = '';
+result307:number=0;
+ 
+ 
+Total307()
+{
+  this.result307 = parseFloat(this.num306) * parseFloat(this.num307);
+
+}
+
+num308:string = '';
+num309:string = '';
+result309:number=0;
+ 
+ 
+Total309()
+{
+  this.result309 = parseFloat(this.num308) * parseFloat(this.num309);
+
+}
+
+
+num400:string = '';
+num401:string = '';
+result401:number=0;
+ 
+ 
+Total401()
+{
+  this.result401 = parseFloat(this.num400) * parseFloat(this.num401);
+
+}
+
+
+
+num402:string = '';
+num403:string = '';
+result403:number=0;
+ 
+ 
+Total403()
+{
+  this.result403 = parseFloat(this.num402) * parseFloat(this.num403);
+
+}
+
+resultall6:number=0;
+ 
+ 
+Totalall6()
+{
+  this.resultall6 = this.result207 + this.result209 + this.result301 + this.result303 + this.result305 + this.result307 + this.result309 + this.result401 + this.result403 ;
+}
+
+
+resultfinal6:number=0;
+ 
+ 
+Totalfinal6()
+{
+  this.resultfinal6 = this.resultall6/22;
+
+}
+
+
+num406:string = '';
+num407:string = '';
+result407:number=0;
+ 
+ 
+Total407()
+{
+  this.result407 = parseFloat(this.num406) * parseFloat(this.num407);
+
+}
+
+
+num408:string = '';
+num409:string = '';
+result409:number=0;
+ 
+ 
+Total409()
+{
+  this.result409 = parseFloat(this.num408) * parseFloat(this.num409);
+
+}
+
+
+
+num500:string = '';
+num501:string = '';
+result501:number=0;
+ 
+ 
+Total501()
+{
+  this.result501 = parseFloat(this.num500) * parseFloat(this.num501);
+
+}
+
+
+num502:string = '';
+num503:string = '';
+result503:number=0;
+ 
+ 
+Total503()
+{
+  this.result503 = parseFloat(this.num502) * parseFloat(this.num503);
+
+}
+
+num504:string = '';
+num505:string = '';
+result505:number=0;
+ 
+ 
+Total505()
+{
+  this.result505 = parseFloat(this.num504) * parseFloat(this.num505);
+
+}
+
+
+num506:string = '';
+num507:string = '';
+result507:number=0;
+ 
+ 
+Total507()
+{
+  this.result507 = parseFloat(this.num506) * parseFloat(this.num507);
+
+}
+
+
+num508:string = '';
+num509:string = '';
+result509:number=0;
+ 
+ 
+Total509()
+{
+  this.result509 = parseFloat(this.num508) * parseFloat(this.num509);
+
+}
+num600:string = '';
+num601:string = '';
+result601:number=0;
+ 
+ 
+Total601()
+{
+  this.result601 = parseFloat(this.num600) * parseFloat(this.num601);
+
+}
+
+num602:string = '';
+num603:string = '';
+result603:number=0;
+ 
+ 
+Total603()
+{
+  this.result603 = parseFloat(this.num602) * parseFloat(this.num603);
+
+}
+
+
+resultall7:number=0;
+ 
+ 
+Totalall7()
+{
+  this.resultall7 = this.result407 + this.result409 + this.result501 + this.result503 + this.result505 + this.result509 + this.result601 + this.result603 ;
+
+}
+
+
+resultfinal7:number=0;
+ 
+ 
+Totalfinal7()
+{
+  this.resultfinal7 = this.resultall7/21;
+
+}
+
+num604:string = '';
+num605:string = '';
+result605:number=0;
+ 
+ 
+Total605()
+{
+  this.result605 = parseFloat(this.num604) * parseFloat(this.num605);
+
+}
+
+num606:string = '';
+num607:string = '';
+result607:number=0;
+ 
+ 
+Total607()
+{
+  this.result607 = parseFloat(this.num606) * parseFloat(this.num607);
+
+}
+
+num608:string = '';
+num609:string = '';
+result609:number=0;
+ 
+ 
+Total609()
+{
+  this.result609 = parseFloat(this.num608) * parseFloat(this.num609);
+
+}
+
+num700:string = '';
+num701:string = '';
+result701:number=0;
+ 
+ 
+Total701()
+{
+  this.result701 = parseFloat(this.num700) * parseFloat(this.num701);
+
+}
+
+
+resultall8:number=0;
+ 
+ 
+Totalall8()
+{
+  this.resultall8 = this.result605 + this.result607 + this.result609 + this.result701 ;
+
+}
+
+resultfinal8:number=0;
+ 
+ 
+Totalfinal8()
+{
+  this.resultfinal8 = this.resultall8/16;
+
+}
+
+
+
+
+resultcgpa:number=0;
+ 
+ 
+Totalcgpa()
+{
+  this.resultcgpa = this.resultfinal/2 + this.resultfinal2/2;
+  
+}
+
+resultcgpa1:number=0;
+ 
+ 
+Totalcgpa1()
+{
+  this.resultcgpa1 = this.resultfinal/3 + this.resultfinal2/3 + this.resultfinal3/3;
+  
+}
+
+resultcgpa2:number=0;
+ 
+ 
+Totalcgpa2()
+{
+  this.resultcgpa2 = this.resultfinal/4 + this.resultfinal2/4 + this.resultfinal3/4 + this.resultfinal4/4;
+  
+}
+
+
+
+resultcgpa3:number=0;
+ 
+ 
+Totalcgpa3()
+{
+  this.resultcgpa2 = this.resultfinal/5 + this.resultfinal2/5 + this.resultfinal3/5 + this.resultfinal4/5 + this.resultfinal5/5;
+  
+}
+
+
+
+resultcgpa4:number=0;
+ 
+ 
+Totalcgpa4()
+{
+  this.resultcgpa4 = this.resultfinal/6 + this.resultfinal2/6 + this.resultfinal3/6 + this.resultfinal4/6 + this.resultfinal5/6 +
+  this.resultfinal6/6;
+  
+}
+
+
+resultcgpa5:number=0;
+ 
+ 
+Totalcgpa5()
+{
+  this.resultcgpa5 = this.resultfinal/7 + this.resultfinal2/7 + this.resultfinal3/7 + this.resultfinal4/7 + this.resultfinal5/7 +
+  this.resultfinal6/7 + this.resultfinal7/7;
+  
+}
+
+resultcgpa6:number=0;
+ 
+ 
+Totalcgpa6()
+{
+  this.resultcgpa6 = this.resultfinal/8 + this.resultfinal2/8 + this.resultfinal3/8 + this.resultfinal4/8 + this.resultfinal5/8 +
+  this.resultfinal6/8 + this.resultfinal7/8 + this.resultfinal8/8;
+  
+}
+
+
+resultmarks:number=0;
+ 
+ 
+Totalmarks()
+{
+  this.resultmarks = this.resultcgpa * 10 - 0.5 * 10;
+
+}
+
+
+resultmarks1:number=0;
+ 
+ 
+Totalmarks1()
+{
+  this.resultmarks1 = this.resultcgpa1 * 10 - 0.5 * 10;
+
+}
+
+
+resultmarks2:number=0;
+ 
+ 
+Totalmarks2()
+{
+  this.resultmarks2 = this.resultcgpa2 * 10 - 0.5 * 10;
+
+}
+
+
+resultmarks3:number=0;
+ 
+ 
+Totalmarks3()
+{
+  this.resultmarks3 = this.resultcgpa3 * 10 - 0.5 * 10;
+
+}
+
+
+
+resultmarks4:number=0;
+ 
+ 
+Totalmarks4()
+{
+  this.resultmarks4 = this.resultcgpa4 * 10 - 0.5 * 10;
+
+}
+
+resultmarks5:number=0;
+ 
+ 
+Totalmarks5()
+{
+  this.resultmarks5 = this.resultcgpa5 * 10 - 0.5 * 10;
+
+}
+
+resultmarks6:number=0;
+ 
+ 
+Totalmarks6()
+{
+  this.resultmarks6 = this.resultcgpa6 * 10 - 0.5 * 10;
+
+}
+
+
+
+}
